d6d0164e67dba0971323dbc71d26e1b45119efe4
mryevhenii07 <mrit.works07@gmail.com>
2022-08-29 18:57:47 +0300

    finished trending

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import CoinSearch from "../components/CoinSearch";
+import Trending from "../components/Trending";
 
 const Home = ({ coins }) => {
   return (
     <div>
       <CoinSearch coins={coins} />
+      <Trending />
     </div>
   );
 };
